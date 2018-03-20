11e8f6057584bf01d7ac0a6bdfb083f8d03c0bd8
thanhnh <thanhnguyenhuu.cs@gmail.com>
2018-03-20 12:05:44 +0800

    Initial Commit

---
+++ udemy_segueDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udemy_segueDemo
+//
+//  Created by Alan on 3/20/18.
+//  Copyright © 2018 Alan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
